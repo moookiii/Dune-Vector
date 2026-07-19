e3a4ee303f62a7b4c31ed066b6d5612b64915da9
Jason <jpallen.0628@gmail.com>
2026-07-19 09:28:46 -0700

    Make drone wing inlays black

--- Assets/DuneVector/Runtime/DuneVectorVisuals.cs
+++ Assets/DuneVector/Runtime/DuneVectorVisuals.cs
@@ -13,6 +13,7 @@ namespace DuneVector
         public Material RivalDroneTop { get; }
         public Material NeutralDroneTop { get; }
         public Material DroneDark { get; }
+        public Material DroneWingInlay { get; }
         public Material Cactus { get; }
         public IReadOnlyList<Material> Shrubs => _shrubMaterials;
         public Material Sandstone { get; }
@@ -85,6 +86,11 @@ namespace DuneVector
                 droneVisuals.FrameColor,
                 droneVisuals.FrameSmoothness,
                 droneVisuals.FrameMetallic);
+            DroneWingInlay = CreateLit(
+                "Drone - Black Wing Inlay",
+                droneVisuals.WingInlayColor,
+                droneVisuals.WingInlaySmoothness,
+                droneVisuals.WingInlayMetallic);
             Cactus = CreateLit("Cactus - Stylized", new Color(0.08f, 0.31f, 0.16f), 0.25f, 0f);
             if (shrubTuning != null)
             {
@@ -320,8 +326,8 @@ namespace DuneVector
                 Quaternion.identity,
                 materials.DroneBody);
 
-            CreateDroneWing(visual, false, settings, materials.DroneBody, topMaterial);
-            CreateDroneWing(visual, true, settings, materials.DroneBody, topMaterial);
+            CreateDroneWing(visual, false, settings, materials.DroneBody, materials.DroneWingInlay);
+            CreateDroneWing(visual, true, settings, materials.DroneBody, materials.DroneWingInlay);
 
             CreatePart(
                 PrimitiveType.Sphere,
