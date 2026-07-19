ce81ea9e1d28c4ba88d1b433123fdad31ad48e17
Jason <jpallen.0628@gmail.com>
2026-07-19 16:24:00 -0700

    Use scene HDRI environment and exposure

--- Assets/DuneVector/Runtime/DuneVectorWeatherSystem.cs
+++ Assets/DuneVector/Runtime/DuneVectorWeatherSystem.cs
@@ -170,8 +170,6 @@ namespace DuneVector
             Camera viewCamera,
             DesertWorldStreamer world,
             Fog fog,
-            GradientSky sky,
-            Exposure exposure,
             DesertWeatherTuning settings)
         {
             settings.EnsureInitialized();
@@ -180,7 +178,7 @@ namespace DuneVector
             GameObject atmosphereObject = new GameObject("HDRP Weather Atmosphere");
             atmosphereObject.transform.SetParent(transform, false);
             _atmosphere = atmosphereObject.AddComponent<DuneVectorWeatherAtmosphere>();
-            _atmosphere.Initialize(fog, sky, exposure, settings.Atmosphere);
+            _atmosphere.Initialize(fog, settings.Atmosphere);
 
             GameObject windObject = new GameObject("Global Desert Wind");
             windObject.transform.SetParent(transform, false);
@@ -220,19 +218,13 @@ namespace DuneVector
     public sealed class DuneVectorWeatherAtmosphere : MonoBehaviour
     {
         private Fog _fog;
-        private GradientSky _sky;
-        private Exposure _exposure;
         private DesertWeatherAtmosphereTuning _settings;
 
         public void Initialize(
             Fog fog,
-            GradientSky sky,
-            Exposure exposure,
             DesertWeatherAtmosphereTuning settings)
         {
             _fog = fog;
-            _sky = sky;
-            _exposure = exposure;
             _settings = settings;
         }
 
@@ -262,20 +254,6 @@ namespace DuneVector
                 _fog.enableVolumetricFog.value = intensity >= _settings.VolumetricFogThreshold;
             }
 
-            if (_sky != null)
-            {
-                _sky.top.value = Color.Lerp(_settings.ClearSkyTop, _settings.StormSkyTop, intensity);
-                _sky.middle.value = Color.Lerp(_settings.ClearSkyMiddle, _settings.StormSkyMiddle, intensity);
-                _sky.bottom.value = Color.Lerp(_settings.ClearSkyBottom, _settings.StormSkyBottom, intensity);
-            }
-
-            if (_exposure != null)
-            {
-                _exposure.fixedExposure.value = Mathf.Lerp(
-                    _settings.ClearExposure,
-                    _settings.StormExposure,
-                    intensity);
-            }
         }
     }
 
